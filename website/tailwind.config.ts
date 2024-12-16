4c541dd8b6dc5bc0c82429562353adf9168fd957
Conor McGeehan <conorm15@gmail.com>
2024-12-16 12:29:12 -0600

    typescript

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -3,11 +3,12 @@ import type { Config } from "tailwindcss";
 const config: Config = {
     darkMode: ["class"],
     content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/**/*.{js,ts,jsx,tsx,mdx}"
+    ],
+    theme: {
   	extend: {
   		colors: {
   			background: 'hsl(var(--background))',
